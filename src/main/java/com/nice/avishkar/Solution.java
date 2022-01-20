2a9294fce29be923710b20772f1ee66140e20027
gouravisnice <gourav.mandhane@nice.com>
2022-01-20 15:02:33 +0530

    First cut of aimers

---
+++ src/main/java/com/nice/avishkar/Solution.java
@@ -0,0 +1,18 @@
+package com.nice.avishkar;
+
+import java.io.IOException;
+import java.nio.file.FileStore;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+public class Solution {
+
+	public ElectionResult execute(Path candidateFile, Path votingFile) throws IOException {
+		ElectionResult resultData = new ElectionResult();
+		
+		// Write code here to read CSV files and process them
+
+		return resultData;
+	}
+
+}
